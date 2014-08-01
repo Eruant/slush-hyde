e1848b88b4ae130744cf197726a5820aca129f84
Matt Gale <matt@littleball.co.uk>
2014-08-01 14:21:13 +0100

    Added jeckll like static file generator

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,34 @@
 var gulp = require('gulp'),
-  jade = require('gulp-jade');
+  frontMatter = require('gulp-front-matter'),
+  marked = require('gulp-marked'),
+  jade = require('jade'),
+  through = require('through2');
 
-gulp.task('default', function () {
+/*globals Buffer*/
+
+var site = {
+  name: 'Test'
+};
+
+var applyTemplate = function (templateFile) {
+
+  return through.obj(function (file, enc, cb) {
+
+    var data = {
+      site: site,
+      page: file.page,
+      content: file.contents.toString()
+    };
+
+    file.contents = new Buffer(jade.renderFile(templateFile, data), 'utf8');
+
+    this.push(file);
+    cb();
+  });
+
+};
+
+gulp.task('old', function () {
 
   return gulp.src([
       'src/**/*.jade',
@@ -10,4 +37,17 @@ gulp.task('default', function () {
     ])
     .pipe(jade())
     .pipe(gulp.dest('www'));
+});
+
+gulp.task('md', function () {
+
+  return gulp.src('src/posts/*.md')
+    .pipe(frontMatter({
+      property: 'page',
+      remove: true
+    }))
+    .pipe(marked())
+    .pipe(applyTemplate('src/templates/post.jade'))
+    .pipe(gulp.dest('tmp'));
+
 });
---
+++ src/posts/test.md
@@ -0,0 +1,7 @@
+---
+title: This is a test
+---
+
+# This is a test
+
+To see if it works
