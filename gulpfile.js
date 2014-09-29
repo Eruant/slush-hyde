0ff029d27a16e64fdf731ecce60ddb8d6b5e87ad
Matt Gale <matt@littleball.co.uk>
2014-09-29 12:26:09 +0100

    Refactoring site functions

--- gulpfile.js
+++ gulpfile.js
@@ -1,95 +1,17 @@
-/*globals Buffer, console*/
+/*globals console*/
 
 var
   browserify = require('browserify'),
   ext = require('gulp-ext-replace'),
   frontMatter = require('gulp-front-matter'),
   gulp = require('gulp'),
-  jade = require('jade'),
   jshint = require('gulp-jshint'),
   marked = require('gulp-marked'),
   sass = require('gulp-sass'),
   source = require('vinyl-source-stream'),
-  through = require('through2'),
   browserSync = require('browser-sync'),
 
-  site = require('./site'),
-
-  applyTemplate = function () {
-
-    return through.obj(function (file, enc, cb) {
-
-      var data = {
-        site: site,
-        page: file.page,
-        content: file.contents.toString()
-      },
-      template = 'src/_templates/' + file.page.template + '.jade';
-
-      file.contents = new Buffer(jade.renderFile(template, data), 'utf8');
-      console.log('applying...', data);
-
-      this.push(file);
-      cb();
-    });
-
-  },
-  
-  collectPosts = function () {
-
-    var posts = [],
-      tags = [];
-
-    return through.obj(function (file, enc, cb) {
-
-      posts.push(file.page);
-      posts[posts.length - 1].content = file.contents.toString();
-
-      if (file.page.tags) {
-        file.page.tags.forEach(function (tag) {
-          if (tags.indexOf(tag) === -1) {
-            tags.push(tag);
-          }
-        });
-      }
-
-      console.log('collecting...');
-
-      this.push(file);
-      cb();
-    }, function (cb) {
-      console.log('sorting...');
-
-      posts.sort(function (a, b) {
-        return b.date - a.date;
-      });
-      site.posts = posts;
-      site.tags = tags;
-      cb();
-    });
-  },
-  
-  collectPages = function () {
-
-    var pages = [];
-
-    return through.obj(function (file, enc, cb) {
-
-      pages.push({
-        title: file.page.title,
-        link: file.page.link
-      });
-
-      cb();
-
-    }, function (cb) {
-
-      site.pages = pages;
-      cb();
-
-    });
-
-  };
+  siteFunc = require('./siteFunctions.js');
 
 gulp.task('collectPosts', function () {
   return gulp.src('src/posts/**/*.md')
@@ -98,7 +20,7 @@ gulp.task('collectPosts', function () {
       remove: true
     }))
     .pipe(marked())
-    .pipe(collectPosts());
+    .pipe(siteFunc.collectPosts());
 });
 
 gulp.task('collectPages', function () {
@@ -111,7 +33,7 @@ gulp.task('collectPages', function () {
       property: 'page',
       remove: true
     }))
-    .pipe(collectPages());
+    .pipe(siteFunc.collectPages());
 
 });
 
@@ -123,7 +45,7 @@ gulp.task('markdown', ['collectPosts', 'collectPages'], function () {
       remove: true
     }))
     .pipe(marked())
-    .pipe(applyTemplate())
+    .pipe(siteFunc.applyTemplate())
     .pipe(ext('.html'))
     .pipe(gulp.dest('dest/test'))
     .pipe(gulp.dest('dest/www'));
---
+++ siteFunctions.js
@@ -0,0 +1,97 @@
+/*globals Buffer*/
+
+// Node modules
+var through = require('through2'),
+  jade = require('jade');
+
+// Local variables
+var site = require('./site');
+
+/**
+ * Take a file stream and render the jade data to html
+ */
+var applyTemplate = function () {
+
+  return through.obj(function (file, enc, cb) {
+
+    var data = {
+      site: site,
+      page: file.page,
+      content: file.contents.toString()
+    },
+    template = 'src/_templates/' + file.page.template + '.jade';
+
+    file.contents = new Buffer(jade.renderFile(template, data), 'utf8');
+
+    this.push(file);
+    cb();
+  });
+
+};
+
+/**
+ * Collects the posts, sorts them and adds them to the site variable
+ */
+var collectPosts = function () {
+
+  // store the changes localy
+  var posts = [],
+    tags = [];
+
+  return through.obj(function (file, enc, cb) {
+
+    // collect the posts
+    posts.push(file.page);
+    posts[posts.length - 1].content = file.contents.toString();
+
+    // store any tags within the pages
+    if (file.page.tags) {
+      file.page.tags.forEach(function (tag) {
+        if (tags.indexOf(tag) === -1) {
+          tags.push(tag);
+        }
+      });
+    }
+
+    this.push(file);
+    cb();
+  }, function (cb) {
+
+    // sort the posts (newest at the top)
+    posts.sort(function (a, b) {
+      return b.date - a.date;
+    });
+    
+    // saves the data to the global site variable
+    site.posts = posts;
+    site.tags = tags;
+    cb();
+  });
+};
+
+var collectPages = function () {
+
+  var pages = [];
+
+  return through.obj(function (file, enc, cb) {
+
+    pages.push({
+      title: file.page.title,
+      link: file.page.link
+    });
+
+    cb();
+
+  }, function (cb) {
+
+    site.pages = pages;
+    cb();
+
+  });
+};
+
+module.exports = {
+  applyTemplate: applyTemplate,
+  collectPosts: collectPosts,
+  collectPages: collectPages
+};
